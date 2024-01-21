e4ba17872814d0753dca4172608b179427407497
CONJON2027 <156270528+CONJON2027@users.noreply.github.com>
2024-01-21 15:56:01 -0500

    All Lessions

---
+++ lessons/basics.py
@@ -0,0 +1,12 @@
+"""Practice with the basics of coding"""
+
+
+print(2)
+print(len('hello'))
+
+
+
+
+
+
+
---
+++ lessons/input.py
@@ -0,0 +1,13 @@
+"""Practice with vars and inputs"""
+
+first_name: str = input("What is your name? ")
+fav_number_str: str = input('What is your favorite number? ')
+fav_number: int = int(fav_number_str)
+higher_number: int = fav_number + 1
+
+print('Hello '+ first_name +'!')
+print('My favorite number is ' + str(higher_number))
+
+print("spooky"[len("spooky")])
+
+
